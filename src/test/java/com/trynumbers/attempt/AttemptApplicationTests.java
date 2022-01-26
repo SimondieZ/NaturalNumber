1cd3dcf97fba756f8cde1241fee4290002d10621
Simon <chaffeeusa@gmail.com>
2022-01-26 22:31:20 +0300

    Initial commit

---
+++ src/test/java/com/trynumbers/attempt/AttemptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trynumbers.attempt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttemptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
